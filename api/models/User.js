35580b0022c15f01a683de75accc50a3b3b0dae7
ojasvisinghal <osinghal00@gmnmail.com>
2017-03-05 12:15:36 +0530

    first sails

---
+++ api/models/User.js
@@ -0,0 +1,33 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+
+    lname: {
+      type: 'string',
+      required: true
+    },
+
+    email:{
+      type: 'string',
+      email: true,
+      unique: true,
+      required: true
+    },
+
+    encryptedPassword:{
+      type: 'string',
+      required: true
+    }
+  }
+};
